70e82b4ef02e9951f37335f9d3dd94be42f6bac6
한규진 <david0218@naver.com>
2022-07-24 23:44:18 +0900

    feat: auth api examples 리액트쿼리 코드들 어케할지 고민

--- src/hooks/apis/useAuthApi.tsx
+++ src/hooks/apis/useAuthApi.tsx
@@ -1,6 +1,8 @@
 import { axiosPrivate, axiosPublic } from '../../config/axios';
 
-export type TMessageSendReqType = { phoneNumber: string };
+export type TMessageSendReqType = {
+  phoneNumber: string;
+};
 export type TMessageSendResType = {
   phoneNumber: string;
   validationNumber: string;
@@ -17,19 +19,37 @@ export type TMessageValidateResType = {
   alreadySingUp: boolean;
 };
 
+export type TRegisterReqType = {
+  name: string;
+};
+export type TRegisterResType = {
+  user: string;
+  accessToken: string;
+};
+
 function useAuthApi() {
+  // 휴대전화 인증번호 요청
   const messageSend = async (data: TMessageSendReqType) => {
-    const res = await axiosPrivate.post('/v1/auth/message/send', data);
+    const res = await axiosPublic.post('/v1/auth/message/send', data);
     console.log(res);
     return res;
   };
 
+  // 인증번호 확인
   const messageValidate = async (data: TMessageValidateReqType) => {
     const res = await axiosPrivate.post('/v1/auth/message/validate', data);
     console.log(res);
     return res;
   };
-  return { messageSend, messageValidate };
+
+  // 회원가입 : 입금자명 입력
+  const register = async (data: TRegisterReqType) => {
+    const res = await axiosPrivate.post('/v1/auth/register', data);
+    console.log(res);
+    return res;
+  };
+
+  return { messageSend, messageValidate, register };
 }
 
 export default useAuthApi;
