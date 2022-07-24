bde516cf8ca1edc4a1cff6e962b166483e6eb5d0
한규진 <david0218@naver.com>
2022-07-24 23:37:45 +0900

    feat: axios, api setting

--- src/hooks/apis/useAuthApi.tsx
+++ src/hooks/apis/useAuthApi.tsx
@@ -1,5 +1,35 @@
+import { axiosPrivate, axiosPublic } from '../../config/axios';
+
+export type TMessageSendReqType = { phoneNumber: string };
+export type TMessageSendResType = {
+  phoneNumber: string;
+  validationNumber: string;
+  alreadySingUp: boolean;
+};
+
+export type TMessageValidateReqType = {
+  phoneNumber: string;
+  validationNumber: string;
+};
+export type TMessageValidateResType = {
+  accessToken: string;
+  registerToken: string;
+  alreadySingUp: boolean;
+};
+
 function useAuthApi() {
-  return {};
+  const messageSend = async (data: TMessageSendReqType) => {
+    const res = await axiosPrivate.post('/v1/auth/message/send', data);
+    console.log(res);
+    return res;
+  };
+
+  const messageValidate = async (data: TMessageValidateReqType) => {
+    const res = await axiosPrivate.post('/v1/auth/message/validate', data);
+    console.log(res);
+    return res;
+  };
+  return { messageSend, messageValidate };
 }
 
 export default useAuthApi;
