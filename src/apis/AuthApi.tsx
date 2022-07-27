c2cc8cc88e26358cd7ca437df098d45251cee598
한규진 <david0218@naver.com>
2022-07-28 01:37:36 +0900

    auth 관련 recoil 세팅, 로직 구현 완료

--- src/apis/AuthApi.tsx
+++ src/apis/AuthApi.tsx
@@ -1,35 +1,42 @@
 import { axiosPrivate, axiosPublic } from './axios';
 import {
   TMessageSendReqType,
+  TMessageSendResType,
   TMessageValidateReqType,
   TMessageValidateResType,
   TRegisterReqType,
+  TRegisterResType,
 } from './type/auth';
+import { TResponseType } from './type/commonResponse';
 
 const AuthApi = {
   // 인증번호 받기
-  messageSend: async (payload: TMessageSendReqType) => {
-    const { data } = await axiosPublic.post('/v1/auth/message/send', payload);
+  messageSend: async (
+    payload: TMessageSendReqType,
+  ): Promise<TResponseType<TMessageSendResType>> => {
+    const { data } = await axiosPublic.post('/auth/message/send', payload);
     console.log(data);
     return data;
   },
 
   // 인증번호 검증
-  messageValidate: async (payload: TMessageValidateReqType) => {
-    const { data }: { data: TMessageValidateResType } = await axiosPrivate.post(
-      '/v1/auth/message/validate',
-      payload,
-    );
+  messageValidate: async (
+    payload: TMessageValidateReqType,
+  ): Promise<TResponseType<TMessageValidateResType>> => {
+    const { data } = await axiosPublic.post('/auth/message/validate', payload);
     console.log(data);
     return data;
   },
 
   // 회원가입 : 입금자명 입력
-  register: async (payload: TRegisterReqType, registerToken: string | null) => {
+  register: async (
+    payload: TRegisterReqType,
+    registerToken: string | null,
+  ): Promise<TResponseType<TRegisterResType>> => {
     // 레지스터토큰 받아서 헤더로 끼고 요청
     if (registerToken) {
-      const { data } = await axiosPrivate.post('/v1/auth/register', payload, {
-        headers: { registerToken },
+      const { data } = await axiosPublic.post('/auth/register', payload, {
+        headers: { registerToken: `Bearer ${registerToken}` },
       });
       console.log(data);
       return data;
